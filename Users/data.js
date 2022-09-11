c66e5d1642f6ea56e1fe9da424257b7c20334441
SuperSky2021 <91467745+SuperSky2021@users.noreply.github.com>
2022-09-11 18:14:12 +0800

    Update data.js

--- Users/data.js
+++ Users/data.js
@@ -1,1 +1,1 @@
-var users = {"Users":{"sky":{"name":"sky","password":"114514"},"debug":{"name":"debug","password":"114514"}}}
+var users = {"Users":{"sky":{"name":"sky","password":"114514","magic_name':"SKY YYDS"},"debug":{"name":"debug","password":"114514"}}}
